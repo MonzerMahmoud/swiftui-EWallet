2cd364d8f7269443006d5153f4f3b25db0d46cc7
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-12-29 00:50:09 +0200

    Initial Commit

---
+++ E-Wallet/E_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_WalletApp.swift
+//  E-Wallet
+//
+//  Created by Syber Expertise on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct E_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
