2ac91504f4a07be44237d75bf8e6d8759517c9e9
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-12-29 05:14:59 +0200

    Init commit

--- E-Wallet/ContentView.swift
+++ E-Wallet/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        FooterBar()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct FooterBar: View {
+    var body: some View {
+        HStack(spacing: 0) {
+            Spacer()
+            Image("wallet")
+                .frame(width: 35, height: 35)
+            Spacer()
+            Image("transactions")
+                .frame(width: 35, height: 35)
+            Spacer()
+            Image("notification-bell")
+                .frame(width: 35, height: 35)
+            Spacer()
+            Image("setting")
+                .frame(width: 35, height: 35)
+            Spacer()
+        }
+        .padding()
+        .background(Color(red: 0.182, green: 0.067, blue: 0.335))
+        .clipShape(Capsule())
+        .frame(maxWidth: 350)
+        .shadow(color:Color(red: 0.833, green: 0.806, blue: 0.872), radius: 4, x: -3, y: 8)
     }
 }
